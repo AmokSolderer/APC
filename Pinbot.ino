32ae175e34e1211c51fd43387a8d257e7ba0917f
AmokSolderer <farimueller@yahoo.de>
2018-02-21 17:38:34 +0100

    Default coil times adjusted

--- Pinbot.ino
+++ Pinbot.ino
@@ -1,6 +1,6 @@
 // Rules for the Pinbot pinball machine
 
-const unsigned int PB_SolTimes[24] = {30,30,30,50,30,800,30,30,500,500,999,999,999,999,500,500,50,500,50,50,50,50,0,0}; // Activation times for solenoids
+const unsigned int PB_SolTimes[24] = {30,30,30,50,30,200,30,30,500,500,999,999,999,999,500,500,50,500,50,50,50,50,0,0}; // Activation times for solenoids
 
 struct SettingTopic PB_setList[4] = {{"DROP TG TIME  ",HandleNumSetting,0,3,10},
        {"RESTOREDEFAULT",RestoreDefaults,0,0,0},
