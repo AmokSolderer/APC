a8c3d829be18ceeb7201f9b4f07a3e59cfef1127
AmokSolderer <farimueller@yahoo.de>
2018-09-28 18:16:39 +0200

    LED control changed

--- APC.ino
+++ APC.ino
@@ -467,89 +467,82 @@ void TC7_Handler() {                                  // interrupt routine - run
   // Lamps
 
   if (!APC_settings[LEDsetting]) {
-    if (LampWait == LampPeriod) {                     // Waiting time has passed
-      LampCol++;                                      // prepare for next lamp column
-      LampColMask = LampColMask<<1;
-      c = 2;                                          // clear buffer
-      REG_PIOC_CODR = AllSelects + AllData;           // clear all select signals and the data bus
-      if (LampCol == 8){                              // max column reached?
-        LampCol = 0;
-        LampColMask = 2;
-        for (i=0; i<8; i++) {                         // write select pattern to buffer
-          if (*(Lamp+i+1)) {
-            REG_PIOC_SODR = c;}
-          c = c<<1;}}
-      else {
-        for (i=0; i<8; i++) {                         // write select pattern to buffer
-          if (*(LampPattern+LampCol*8+i+1)) {
-            REG_PIOC_SODR = c;}
-          c = c<<1;}}
-      REG_PIOC_SODR = 33554432;                       // use Sel1
-      REG_PIOC_CODR = AllSelects + AllData;           // clear all select signals and the data bus
-      REG_PIOC_SODR = LampColMask;
-      LampWait = 1;                                  	// restart lamp waiting counter
-      REG_PIOC_SODR = 268435456;}                     // use Sel0
-    else {
-      if (APC_settings[DimInserts]) {                 // if inserts have to be dimmed
-        REG_PIOC_CODR = AllSelects + AllData;         // clear all select signals and the data bus
-        REG_PIOC_SODR = 268435456;}                   // use Sel0 to disable column driver outputs at half time
-      LampWait++;}}                                   // increase wait counter
+  	if (LampWait == LampPeriod) {                     // Waiting time has passed
+  		LampCol++;                                      // prepare for next lamp column
+  		LampColMask = LampColMask<<1;
+  		c = 2;                                          // clear buffer
+  		REG_PIOC_CODR = AllSelects + AllData;           // clear all select signals and the data bus
+  		if (LampCol == 8){                              // max column reached?
+  			LampCol = 0;
+  			LampColMask = 2;
+  			for (i=0; i<8; i++) {                         // write select pattern to buffer
+  				if (*(Lamp+i+1)) {
+  					REG_PIOC_SODR = c;}
+  				c = c<<1;}}
+  		else {
+  			for (i=0; i<8; i++) {                         // write select pattern to buffer
+  				if (*(LampPattern+LampCol*8+i+1)) {
+  					REG_PIOC_SODR = c;}
+  				c = c<<1;}}
+  		REG_PIOC_SODR = 33554432;                       // use Sel1
+  		REG_PIOC_CODR = AllSelects + AllData;           // clear all select signals and the data bus
+  		REG_PIOC_SODR = LampColMask;
+  		LampWait = 1;                                  	// restart lamp waiting counter
+  		REG_PIOC_SODR = 268435456;}                     // use Sel0
+  	else {
+  		if (APC_settings[DimInserts]) {                 // if inserts have to be dimmed
+  			REG_PIOC_CODR = AllSelects + AllData;         // clear all select signals and the data bus
+  			REG_PIOC_SODR = 268435456;}                   // use Sel0 to disable column driver outputs at half time
+  		LampWait++;}}                                   // increase wait counter
   else {                                              // LEDs selected
-    if (LampCol > 19) {                               // 20ms over
-      LampCol = 0;}                                   // start from the beginning
-    if (LampCol < 8) {                                // the first 8 cycles are for transmitting the status of the lamp matrix
-      REG_PIOC_CODR = AllData;
-      c = 2;
-      if (!LampCol){                                  // max column reached?
-        for (i=0; i<8; i++) {                         // write select pattern to buffer
-          if (*(Lamp+i+1)) {
-            REG_PIOC_SODR = c;}
-          c = c<<1;}}
-      else {
-        for (i=0; i<8; i++) {                         // write select pattern to buffer
-          if (*(LampPattern+LampCol*8+i+1)) {
-            REG_PIOC_SODR = c;}
-          c = c<<1;}}
-      if (LEDFlag) {
-        LEDFlag = false;
-        REG_PIOC_CODR = Sel5;}                        // activate Sel5 falling edge
-      else {
-        LEDFlag = true;
-        REG_PIOC_SODR = Sel5;}}                       // activate Sel5 rising edge
-    else {                                            // the lamp matrix is already sent
-      if (LampCol < 17) {                             // still time to send a command?
-        if (LEDCommandBytes) {                        // are there any pending LED commands?
-          REG_PIOC_CODR = AllData;
-          REG_PIOC_SODR = (LEDCommand[LEDCount])<<1;  // send the first byte
-          LEDCount++;                                 // increase the counter
-          if (LEDCount != LEDCommandBytes) {          // not all command bytes sent?
-            LEDByteBuf = LEDCommand[LEDCount];        // put the next byte into the buffer
-            LEDCount++;                               // increase the counter
-            //LEDFlag = true;                           // indicate there's a byte in the buffer
-            if (LEDCount == LEDCommandBytes) {        // all command bytes sent?
-              LEDCommandBytes = 0;                    // clear indicator
-              LEDCount = 0;}}                         // and counter
-          else {
-            LEDCommandBytes = 0;
-            LEDCount = 0;}
-          if (LEDFlag) {
-            LEDFlag = false;
-            REG_PIOC_CODR = Sel5;}                    // activate Sel5 falling edge
-          else {
-            LEDFlag = true;
-            REG_PIOC_SODR = Sel5;}}}
-      else {                                          // this must be LampCol 36
-        if (LampCol == 17) {
-          REG_PIOC_CODR = AllData;
-          REG_PIOC_SODR = 170<<1;                     // time to sync
-          REG_PIOC_SODR = Sel5;
-        if (LEDFlag) {
-          LEDFlag = false;
-          REG_PIOC_CODR = Sel5;}                      // activate Sel5 falling edge
-        else {
-          LEDFlag = true;
-          REG_PIOC_SODR = Sel5;}}}}
-    LampCol++;}
+  	if (LampCol > 19) {                               // 20ms over
+  		LampCol = 0;}                                   // start from the beginning
+  	if (LampCol < 8) {                                // the first 8 cycles are for transmitting the status of the lamp matrix
+  		REG_PIOC_CODR = AllData;
+  		c = 2;
+  		if (!LampCol){                                  // max column reached?
+  			for (i=0; i<8; i++) {                         // write select pattern to buffer
+  				if (*(Lamp+i+1)) {
+  					REG_PIOC_SODR = c;}
+  				c = c<<1;}}
+  		else {
+  			for (i=0; i<8; i++) {                         // write select pattern to buffer
+  				if (*(LampPattern+LampCol*8+i+1)) {
+  					REG_PIOC_SODR = c;}
+  				c = c<<1;}}
+  		if (LEDFlag) {
+  			LEDFlag = false;
+  			REG_PIOC_CODR = Sel5;}                        // activate Sel5 falling edge
+  		else {
+  			LEDFlag = true;
+  			REG_PIOC_SODR = Sel5;}}                       // activate Sel5 rising edge
+  	else {                                            // the lamp matrix is already sent
+  		if (LampCol < 13) {                             // still time to send a command?
+  			if (LEDCommandBytes) {                        // are there any pending LED commands?
+  				REG_PIOC_CODR = AllData;
+  				REG_PIOC_SODR = (LEDCommand[LEDCount])<<1;  // send the first byte
+  				LEDCount++;                                 // increase the counter
+  				if (LEDCount == LEDCommandBytes) {          // not all command bytes sent?
+  					LEDCommandBytes = 0;
+  					LEDCount = 0;}
+  				if (LEDFlag) {
+  					LEDFlag = false;
+  					REG_PIOC_CODR = Sel5;}                    // activate Sel5 falling edge
+  				else {
+  					LEDFlag = true;
+  					REG_PIOC_SODR = Sel5;}}}
+  		else {                                          // this must be LampCol 36
+  			if (LampCol == 17) {
+  				REG_PIOC_CODR = AllData;
+  				REG_PIOC_SODR = 170<<1;                     // time to sync
+  				REG_PIOC_SODR = Sel5;
+  				if (LEDFlag) {
+  					LEDFlag = false;
+  					REG_PIOC_CODR = Sel5;}                      // activate Sel5 falling edge
+  				else {
+  					LEDFlag = true;
+  					REG_PIOC_SODR = Sel5;}}}}
+  	LampCol++;}
     
 	// Sound
 
