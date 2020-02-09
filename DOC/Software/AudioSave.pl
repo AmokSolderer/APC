2daf2e87e37f534c7edda001982ee0f5b04be18b
AmokSolderer <farimueller@yahoo.de>
2020-02-09 14:17:31 +0100

    AudioSave updated to ignore meta data

--- DOC/Software/AudioSave.pl
+++ DOC/Software/AudioSave.pl
@@ -2,30 +2,50 @@
 
 $buf1 = 0;
 $buf2 = 0;
+$i = 0;
+$size = 0;
+@data = ('d','a','t','a');                  # 'data' is indicating the beginning of the data chunk
 
 open(FH, "< Data.wav");
 binmode(FH);
-open(FOUT, "> Data.BIN");
+open(FOUT, "> Data.bin");
 binmode(FOUT);
 
-for (my $i = 0; $i < 44; $i++) {
-    read(FH, $buf1, 1);}                    # skip the Header
-
-until (eof(FH)) {
+do {                                        # search for the 'data' keyword
+    read(FH, $buf1, 1);                     # read charakter
+    if ($buf1 eq $data[$i]) {               # does it belong to the keyword?
+        $i++;}                              # increase hit counter
+    else {
+        $i = 0;}                            # reset hit counter
+    } until ($i > 3);                       # all four characters found?
+read(FH, $buf1, 1);                         # read LSByte of the data chunk size
+$size = ord($buf1);
+read(FH, $buf1, 1);
+$size = $size | (ord($buf1) << 8);
+read(FH, $buf1, 1);
+$size = $size | (ord($buf1) << 16);
+read(FH, $buf1, 1);                         # read MSByte of the data chunk size
+$size = $size | (ord($buf1) << 24);
+$i = 0;
+do {                                        # read all bytes of the data chunk
     read(FH, $buf1, 1);                     # the read uses chars
+    $i++;
     read(FH, $buf2, 1);                     # get two two have a 16 bit value
-    #read(FH, $buf1, 1);                     # uncomment this line and the next when using 
+    $i++;
+    #read(FH, $buf1, 1);                     # uncomment these four lines when using 
+    #$i++;
     #read(FH, $buf2, 1);                     # stereo input files to skip one channel
-    $buf1 = unpack("C", $buf1);             # convert to numbers
-    $buf2 = unpack("C", $buf2);
+    #$i++;
+    $buf1 = ord($buf1);                     # convert to numbers
+    $buf2 = ord($buf2);
     $buf = ($buf2 << 8) | $buf1; 						# and to 16 bit
     $buf = $buf >> 4;                       # reduce to 12 bit
     if ($buf < 2048) {											# move the zero to 2048 
         $buf = $buf + 2048;}
     else {
         $buf = $buf - 2048;}
-    syswrite (FOUT, pack("v", $buf), 2);}   # and fumble it into the output file
-
+    syswrite (FOUT, pack("v", $buf), 2);    # and fumble it into the output file
+    } while ($i < $size);                   # all byte of the data chunk read?
 close(FH);
 close(FOUT);
 
