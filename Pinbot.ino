798d84ca805189e9e8c580c6e6b90f69e43743f0
ff <ff@192.168.1.248>
2023-12-09 13:21:51 +0100

    PB_Congrats finalized

--- Pinbot.ino
+++ Pinbot.ino
@@ -34,7 +34,7 @@ const byte PB_MultiballSeq[69] = {16,5,15,5,26,5,29,10,26,5,15,5,16,10,15,5,26,5
 const byte PB_ScoreEnergySeq[7] = {31,10,31,10,31,10,0};
 const byte PB_LeftBBinserts[5] = {28, 20, 28, 20, 0};
 const byte PB_RightBBinserts[5] = {27, 20, 27, 20, 0};
-const byte PB_BB_FlasherCycle[17] = {32, 6, 30, 2, 31, 8, 29, 8, 26, 8, 15, 8, 16, 3, 9, 1, 0};
+const byte PB_BB_FlasherCycle[15] = {32, 6, 30, 2, 31, 8, 29, 8, 26, 8, 15, 8, 16, 3, 0};
 const byte PB_Ball_Locked[5] = {26,30,26,30,0};
 const byte PB_SkillShotFail[25] = {26,10,15,40,26,10,15,40,26,10,15,40,26,10,15,40,26,10,15,40,26,10,15,40,0};
 const byte PB_MultiplierSeq[83] = {27,2,29,6,26,7,27,6,15,5,16,8,27,5,29,8,30,5,26,1,31,11,28,1,15,6,31,3,16,3,28,8,27,1,29,9,26,5,27,4,15,4,31,7,16,3,31,7,26,1,29,10,32,1,15,11,30,1,16,4,31,3,29,8,28,6,26,5,31,7,15,1,16,11,29,10,31,2,15,6,16,4,0};
@@ -345,9 +345,10 @@ const byte PB_WalkingLines[199] = {15,0b01000,0b01000,0b01000,0b01000,0b01000,
                                    15,0b10000,0b01000,0b01000,0b00100,0b00100,
                                    15,0b10000,0b10000,0b10000,0b10000,0b10000,0};
 
-const byte PB_Characters[180] = {12,18,30,18,18,28,18,28,19,28,14,16,16,16,14,28,18,18,18,28,30,16,28,16,30,30,16,28,16,16,14,16,22,18,14,18,18,30,18,18,4,4,4,4,4,14,2,2,10,4,18,20,24,20,18, // a,b,c,d,e,f,g,h,i,j,k
+const byte PB_Characters[185] = {0,0,0,0,0,12,18,18,18,12,4,12,4,4,14,12,18,4,8,30,12,2,12,2,12,10,18,30,2,2,28,16,28,2,12,12,16,28,18,12,30,2,4,8,8,12,18,12,18,12,12,18,14,2,12, // Blank,0,1,2,3,4,5,6,7,8,9
+                                 12,18,30,18,18,28,18,28,18,28,14,16,16,16,14,28,18,18,18,28,30,16,28,16,30,30,16,28,16,16,14,16,22,18,14,18,18,30,18,18,4,4,4,4,4,14,2,2,10,4,18,20,24,20,18, // a,b,c,d,e,f,g,h,i,j,k
                                  8,8,8,8,14,17,27,21,17,17,17,25,21,19,17,12,18,18,18,12,28,18,28,16,16,12,18,18,22,12,28,18,28,20,18,14,16,12,2,28,14,4,4,4,4,18,18,18,18,12,17,17,10,10,4, // l,m,n,o,p,q,r,s,t,u,v
-                                 17,17,21,21,10,17,10,4,10,17,17,10,4,4,4,31,2,4,8,31,12,18,18,18,12,4,12,4,4,14,12,18,4,8,30,12,2,12,2,12,10,18,30,2,2,28,16,28,2,12,12,16,28,18,12,30,2,4,8,8,12,18,12,18,12,12,18,14,2,12}; // w,x,y,z,0,1,2,3,4,5,6,7,8,9
+                                 17,17,21,21,10,17,10,4,10,17,17,10,4,4,4,31,2,4,8,31}; // w,x,y,z
 
 struct GameDef PB_GameDefinition = {
     PB_setList,                                       // GameSettingsList
@@ -2964,12 +2965,8 @@ void PB_EnterInitials(byte Switch) {
     KillTimer(ByteBuffer2);
     ByteBuffer++;
     if (ByteBuffer > 2) {
-      if (APC_settings[Volume]) {
-        ByteBuffer3 = APC_settings[Volume];
-        FadeOutMusic(100);
-        ActivateTimer(1000, 0, PB_EnterInitials2);}   // TODO fix
-      else {
-        StopPlayingMusic();}}
+      FadeOutMusic(100);
+      ActivateTimer(1000, 0, PB_EnterInitials2);}
     else {
       PB_BlinkInitial(1);}
     break;
@@ -3005,13 +3002,20 @@ void PB_BlinkInitial(byte State) {                    // blink actual character
     PB_ClearChest();
     State = 0;}
   else {
+    byte Buffer = 0;
     for (byte i=0; i<3; i++) {
       *(DisplayUpper+20+4*i) = DispPattern1[(EnterIni[i]-32)*2];
       *(DisplayUpper+21+4*i) = DispPattern1[(EnterIni[i]-32)*2+1];}// restore the characters
-    for (byte x=0; x<5; x++) {                        // for all rows
+    if (EnterIni[ByteBuffer] == 32) {                 // It's a blank
+      Buffer = 0;}
+    else if (EnterIni[ByteBuffer] < 65) {             // It's a number
+      Buffer = EnterIni[ByteBuffer] - 47;}
+    else {                                            // It's a letter
+      Buffer = EnterIni[ByteBuffer] - 54;}
+    for (byte x=0; x<5; x++) {                        // for all chest rows
       byte Mask = 16;                                 // mask to access the stored lamps for this player
       for (byte y=0; y<5; y++) {                      // for all columns
-        if (PB_Characters[(EnterIni[ByteBuffer]-65)*5+x] & Mask) {
+        if (PB_Characters[Buffer*5+x] & Mask) {
           TurnOnLamp(28+x+8*y);}
         else {
           TurnOffLamp(28+x+8*y);}
